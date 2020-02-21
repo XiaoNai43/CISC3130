13972c24daba1a0ad5464861f215f31bf2c0e265
Xiao Nai <xiaonai.he43@gmail.com>
2020-02-21 00:22:01 -0500

    cleaned up the code

--- AssignmentOne/src/streamingArtists.java
+++ AssignmentOne/src/streamingArtists.java
@@ -4,11 +4,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class streamingArtists {
     public static void main(String[] args) throws IOException {
         List<String> data = null; 
+        
+        //try-catch to read the input from .csv file into the data List
         try {
             Path input = Paths.get("../data/global.csv");
             data = Files.readAllLines(input);
@@ -20,23 +21,39 @@ public class streamingArtists {
         String[][] artistsInfo = new String[data.size()][];
         int index = 0;
 
+        //loops through the data and and spilts it at ","
         for (String value : data) {
-            artistsInfo[index++] = value.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            //ignores the "," in the quotation marks
+            artistsInfo[index++] = value.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); 
         } //END for loop
 
-
+        //Creats HashMap
         Map<Artist, Integer> artists = new HashMap<>();
 
+        //Puts the artist names into HashMap
         for (String[] artistInfo : artistsInfo) {
-                Artist artist = new Artist(artistInfo[2]);
-                Integer j = artists.get(artist);
-                artists.put(artist, (j == null) ? 1 : j + 1);
-            }
+            Artist artist = new Artist(artistInfo[2]);
+            Integer j = artists.get(artist);
+            artists.put(artist, (j == null) ? 1 : j + 1);
+        }
 
-        List<String> top10 = artists.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> -e.getValue())).limit(10).map(Object::toString).collect(Collectors.toList());
-        Path path = Paths.get("top10.txt");
-        Files.write(path,top10);
+        /*
+        * Creates a list topArtists
+        * Sorts the list by the amount of times 
+        * they apeared in the list
+        */
+        List<String> topArtists = artists.entrySet().stream()
+                .sorted(Comparator.comparingInt(e -> -e.getValue())).limit(200).
+                map(Object::toString).collect(Collectors.toList());
+        
+        topArtists.sort(Comparator.comparing(String::toString));
+        
+        //Creates a new topArtists.txt file if it doesn't exsist already
+        Path path = Paths.get("topGlobal.txt");
+        //writes the topArtist list to the topArtists.txt file
+        Files.write(path,topArtists);
+
+
     }// END main method
 } //END class
 
@@ -44,6 +61,7 @@ public class streamingArtists {
 class Artist {
     private String name;
 
+    //Constructor
     Artist(String artist) {
         this.name = artist;
     }
@@ -61,8 +79,8 @@ class Artist {
         if (this == obj) {
             return true;
         }
-
-        if (obj == null || getClass() != obj.getClass()) {
+        
+        if (obj == null || getClass() != obj.getClass()) { 
             return false;
         }
         
@@ -75,16 +93,8 @@ class Artist {
         return Objects.hash(name);
     }
 
-    // @Override
-    // public String toString() {
-    //     return "Artist{" + 
-    //             "name='" + name + '\'' +
-    //             '}';
-    // }
-
     @Override
     public String toString() {
-        return name;
+        return name.replaceAll("\"", "");
     }   
-    
 }
